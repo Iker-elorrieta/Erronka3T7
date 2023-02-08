a12c4b3595c188f367df145f4d1132d9f53cf312
Andoni <andoni.fernandezlo@elorrieta-errekamari.com>
2023-02-08 13:48:45 +0100

    Andoni/08/13.48

--- RETO3T7/src/Test/MetodosTest.java
+++ RETO3T7/src/Test/MetodosTest.java
@@ -1,13 +1,8 @@
 package Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.sql.Date;
 
 import org.junit.jupiter.api.Test;
 
-import Objetos.Aretoak;
-import Objetos.Filmak;
 
 class MetodosTest {
 	
